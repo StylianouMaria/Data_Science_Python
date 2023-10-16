a2f6c8e3927be9d4f8ef02d1f7be6822fe962749
Maria <stylianoumaria9@gmail.com>
2023-10-16 18:35:08 +0100

    Add examples of data visualisation and notes

---
+++ Data_Visualisation/Data_Visualisation_2/Data_Visualisation_Notes.py
@@ -0,0 +1,57 @@
+#Data Visualisation with Python - I
+
+#DATA WRANGLING: Process of identify, collect, merge and preprocess
+#one or more datasets in preparation for data cleaning
+
+#DATA CLEANING: fixng errors, dealing with missing data, correct formatting
+#fixing and removing incorrect data
+
+#DATA PREPARATION: normalisation for machine learning, understanidng 
+#visualisation is essential for the above techniques
+
+#DATA VISUALISATION:
+#Bar chart or comumn chart: 
+'''
+• Discrete data: data that can be counted
+• Compares values across categories
+• Best for categorical data (few categories)
+• Colour coded categories for patterns to stand out
+'''
+
+#Line graph: 
+'''
+• Shows values over time
+• Multiple values can be shown from different categories
+• Best for linear/continuous data, time series data
+• Lines can be colour coded
+• Time if often the x-axis
+'''
+
+#Pie Chart:
+'''
+• Represents the proportion of the quantity
+• Sum is 100%, Have no more than 10 segments
+• easy to separate categories
+'''
+
+#Pyramid chart:
+'''
+• Hierarchical visualisation, shows the relationships through the stacks
+• Best for different sub-categories and works non-specific numerical values 
+'''
+
+
+#Notes:
+'''
+1. Clean and preprocess data before visualisation.
+2. Lear about the datam to plan the visualisation techniques
+3. Determine the goals of the projects, what u are trying to find
+- Sometimes this is not known at the beginning of the analysis
+4. Data visualisation: What type of data you are using?
+5. Refine the visualisation: make them more aesthetic pleasing and understandable
+6. Write down the findings and observation/ questions, original goals
+'''
+
+
+
+
---
+++ Data_Visualisation/Data_Visualisation_2/Data_visualisation_Notes2.py
@@ -0,0 +1,24 @@
+#Data Visualisation with Python - II
+
+#Scatterplot or x-y graph: 
+'''
+• Able to identify trends and relationships between 2 variables
+• Data characterised by numerical values, 2 variables
+• Not useful for categorical data, or time series
+'''
+
+
+#Scatterplot matrix: 
+'''
+• Assist to determine relationships between multiple variables
+• Best for data characterised by numerical and multiple variables
+• Not useful for time series
+'''
+
+
+#Double axis chart or Dual-axis chart: 
+'''
+• Can be bar chart owith line chart or multiple line on visualisation
+• Can be used for time series, discrete or contrinuous data
+• But can be overwhelming
+'''
